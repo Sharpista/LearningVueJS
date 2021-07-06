add0e7198ce462e9229390062a517b5800ebebf6
ALEXANDRE FILHO <alexandre.filho@radixeng.com.br>
2021-07-05 22:59:27 -0300

    adicionando arquivos

--- componentes/app-intro/src/Contador.vue
+++ componentes/app-intro/src/Contador.vue
@@ -1,9 +1,9 @@
 <template>
-<div class="contador">
-    <span>{{contador}}</span>
-    <button @click="adicionar">+</button>
-    <button @click="subtrair">-</button>
-</div>
+    <div class="contador">
+        <span>{{contador}}</span>
+        <button @click="adicionar">+</button>
+        <button @click="subtrair">-</button>
+    </div>
 </template>
 
 <script>
