3bfd46582d866e806108b8b5ded8bfecb07dcd4a
Alexandre Roberto dos Santos Filho <alexandre.filho@radixeng.com.br>
2021-07-05 22:16:09 -0300

    adicionando arquivos

---
+++ diretivas-exercicios/diretivas-exercicios/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+	<div id="app">
+		<h1>Diretivas</h1>
+		<p v-text="'Usando diretivas v-text'"></p>
+		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
+		<hr>
+		<p v-destaque:fundo.atrasar="'lightblue'">Usando diretiva personalizada </p>
+		<p v-destaque.atrasar="cor">Usando diretiva personalizada </p>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			cor :'purple'
+		}
+	},
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+</style>
