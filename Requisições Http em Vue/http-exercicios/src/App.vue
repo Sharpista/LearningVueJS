e4f431a8ae2501eccf5d580f424568a3e29e2c0e
Alexandre Roberto dos Santos Filho <alexandre.filho@radixeng.com.br>
2021-07-21 13:21:40 -0300

    criando um form

--- Requisições Http em Vue/http-exercicios/src/App.vue
+++ Requisições Http em Vue/http-exercicios/src/App.vue
@@ -1,6 +1,24 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome">
+				 <b-form-input type="text" size="lg" v-model="usuario.nome" placeholder="Informe o Nome">
+				 </b-form-input>
+			</b-form-group>
+			<b-form-group label="Email">
+				 <b-form-input type="email" size="lg" v-model="usuario.email" placeholder="Informe o Email">
+				 </b-form-input>
+			</b-form-group>
+			<hr>
+			<b-button @click="salvar" size="lg" variant="primary">
+				Salvar
+			</b-button>
+		</b-card>
 	</div>
 </template>
 
@@ -12,6 +30,20 @@ export default {
 	// 		email: 'alexandre@alexandre.com'
 	// 	}).then(res => console.log(res))
 	// }
+
+	data(){
+		return{
+			usuario:{
+				nome:'',
+				email:''
+			}
+		}
+	},
+	methods:{
+		salvar(){
+			
+		}
+	}
 }
 </script>
 
