95af6645acb718364231f4071cc0972555f0969e
Alexandre Roberto dos Santos Filho <alexandre.filho@radixeng.com.br>
2021-08-11 11:36:18 -0300

    Lendo e usando parametros de rotas

--- Rotas em uma Aplicação VueJS/rotas-exercicios/src/components/template/Menu.vue
+++ Rotas em uma Aplicação VueJS/rotas-exercicios/src/components/template/Menu.vue
@@ -3,7 +3,7 @@
       <ul>
         <router-link to="/" tag="li" active-class="active" exact>
         <a>Inicio</a> </router-link>
-        <router-link to="/Usuario" tag="li" active-class="active"> 
+        <router-link to="/Usuario/10" tag="li" active-class="active"> 
         <a>Usuario</a> </router-link>
       </ul>
   </nav>
--- Rotas em uma Aplicação VueJS/rotas-exercicios/src/components/usuario/Usuario.vue
+++ Rotas em uma Aplicação VueJS/rotas-exercicios/src/components/usuario/Usuario.vue
@@ -2,13 +2,18 @@
   <div class="usuario">
       <h2>Usuarios</h2>
       <hr>
+      <p><strong>Código: </strong>{{ id }}</p>
       <button sucesso @click="irParaInicio">Voltar</button>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+      return {
+        id: this.$route.params.id
+      }
+    },
   methods: {
     irParaInicio(){
       this.$router.push('/')
