bc82e34048948218432936633eb1fefbbbb038e9
Alexandre Roberto dos Santos Filho <alexandre.filho@radixeng.com.br>
2021-08-11 12:41:33 -0300

    usando query na URL

--- Rotas em uma Aplicação VueJS/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
+++ Rotas em uma Aplicação VueJS/rotas-exercicios/src/components/usuario/UsuarioDetalhe.vue
@@ -3,7 +3,9 @@
       <h3>Usuário Detalhe</h3>
       <!-- :to="`/usuario/${id}/editar`"-->
       <p><strong>Código: </strong>{{ id }}</p>
-      <router-link tag="button" primario :to="{name : 'editarUsuario', params:{id}}" >
+      <router-link tag="button" primario 
+                  :to="{name : 'editarUsuario', params:{id}, 
+                  query: {completo:true, lingua:'pt'}}" >
         Editar
       </router-link>
   </div>
