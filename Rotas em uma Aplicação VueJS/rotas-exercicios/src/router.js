9cc3ed7e8bd51136ef682556150e525c4b08c118
Alexandre Roberto dos Santos Filho <alexandre.filho@radixeng.com.br>
2021-08-17 12:15:49 -0300

    redirencionamento

--- Rotas em uma Aplicação VueJS/rotas-exercicios/src/router.js
+++ Rotas em uma Aplicação VueJS/rotas-exercicios/src/router.js
@@ -32,5 +32,9 @@ export default new Router({
             {path:':id', component: UsuarioDetalhe, props:true},
             {path:':id/editar', component:UsuarioEditar, props:true , name:'editarUsuario'},
         ]
+    },
+    {
+        path : '/redirecionar',
+        redirect : '/usuario'
     }]
 })
