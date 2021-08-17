90f3ac1134ab800af0ad942ea0bc94517fe5cdb6
Alexandre Roberto dos Santos Filho <alexandre.filho@radixeng.com.br>
2021-08-17 12:38:10 -0300

    Passando Fragmento Hash

--- Rotas em uma Aplicação VueJS/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
+++ Rotas em uma Aplicação VueJS/rotas-exercicios/src/components/usuario/UsuarioEditar.vue
@@ -4,7 +4,12 @@
       <p><strong>Código: </strong>{{ id }}</p>
       <p><strong>Completo:</strong>{{$route.query.completo ? 'Sim' :'Não'}}</p>
       <p><strong>Lingua: </strong>{{$route.query.lingua}}</p>
+
+      <div id="rodape">
+        <h3>Curso Vue</h3>
+      </div>
   </div>
+  
 </template>
 
 <script>
@@ -14,5 +19,7 @@ export default {
 </script>
 
 <style>
-
+#rodape{
+  margin-top: 1000px;
+}
 </style>
